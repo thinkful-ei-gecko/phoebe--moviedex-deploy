561f0b5e80925f19d06d2b99af91d382b10bce00
Phoebe Law <phoebebasilio@gmail.com>
2019-09-24 15:08:54 -0700

    Adds more client-side validation. Adds API key authentication/authorization, general security/support for CORS.

--- server.js
+++ server.js
@@ -1,52 +1,98 @@
+'use strict';
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const movies = require('./movies.json');
 
 const server = express();
 
 server.use(morgan('dev'));
+server.use(helmet());
+server.use(cors());
 
-server.use('/movie', (req, res) => {
-    const { genre = '', country = '', avg_vote = '' } = req.query;
+server.use(function validateBearerToken (req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-    const validGenres = ['Animation', 'Drama', 'Comedy', 'Romantic', 'Drama', 'Crime', 'Horror', 'Documentary', 'Action', 'Thriller', 'Adventure', 'Fantasy', 'Musical', 'Biography', 'History', 'War', 'Grotesque', 'Western', 'Spy']
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res
+      .status(401)
+      .json({ error: 'Unauthorized request' })
+  }
+  next();
+});
 
-    function handleGetTypes(req, res) {
-        res.json(validGenres)
+server.get('/movie', function handleGetMovies (req, res) {
+  //1. retrieve the parameter values from the query object
+  const { genre = '', country = '', avg_vote = '' } = req.query;
+
+  //2. validate the parameter values, never trust the client
+
+  function validateString (input) {
+    if (typeof input !== 'string') {
+      return res
+        .status(400)
+        .json({ error: 'Input must be a string'});
     }
+  }
 
-    let results = movies;
+  //VALIDATE GENRES
+  validateString(genre); //checks if genre is a string
 
-    if (genre) {
-        results = movies.filter(movie =>
-          movie
-            .genre.toLowerCase()
-            .includes(genre.toLowerCase())
-        );
-    res.send(results)
+  const validGenres = ['Animation', 'Drama', 'Comedy', 'Romantic', 'Drama', 'Crime', 'Horror', 'Documentary', 'Action', 'Thriller', 'Adventure', 'Fantasy', 'Musical', 'Biography', 'History', 'War', 'Grotesque', 'Western', 'Spy'];
+  const lowercaseValidGenres = validGenres.map(genreName => genreName.toLowerCase());
+  if (!lowercaseValidGenres.includes(genre.toLowerCase)) {
+    return res
+      .status(400)
+      .json({ error: 'Genre must be one of the following: Animation, Drama, Comedy, Romantic, Drama, Crime, Horror, Documentary, Action, Thriller, Adventure, Fantasy, Musical, Biography, History, War, Grotesque, Western, Spy' });
+  } //checks if genre is a valid string
+
+  //VALIDATE COUNTRY is a string
+  validateString(country);
+
+  //VALIDATE AVERAGE VOTE is a number
+  const avgVoteNum = Number(avg_vote);
+  function validateNumber (input) {
+    if (typeof input !== 'number') {
+      return res
+        .status(400)
+        .json({ error: 'Input must be a number'});
     }
+  }
+  validateNumber(avgVoteNum);
+ 
+  //4. if the values are valid, process the request.
+  let results = movies;
 
-    if (country) {
-        results = movies.filter(movie =>
-          movie
-            .country.toLowerCase()
-            .includes(country.toLowerCase())
-        );
-    res.send(results)
-    }
+  if (genre) {
+    results = movies.filter(movie =>
+      movie
+        .genre.toLowerCase()
+        .includes(genre.toLowerCase())
+    );
+  }
 
-    if (avg_vote) {
-        const avg_voteNum = Number(avg_vote);
-        results = movies.filter(movie =>
-          movie.avg_vote >= avg_voteNum
-        );
-    res.send(results)
-    }
+  if (country) {
+    results = movies.filter(movie =>
+      movie
+        .country.toLowerCase()
+        .includes(country.toLowerCase())
+    );
+  }
 
-})
+  if (avgVoteNum) {
+    results = movies.filter(movie =>
+      movie.avg_vote >= avgVoteNum
+    );
+    // res.send(results)
+  }
 
-
+  //5. construct and 6. send a response
+  res.send(results);
+});
 
 server.listen(8000, 
-    console.log('Server is running at port 8000')
+  console.log('Server is running at port 8000')
 );
